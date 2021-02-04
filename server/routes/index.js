fa345cf2b42ed31f408a70be9cda07ed2f6e70b7
Edwin Dharmawan <edwindharmawan@gmail.com>
2021-02-04 21:05:33 +0700

    add: weather APIs

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
 const userRouter = require('./userRouter')
 const songRouter = require('./songRouter')
+const weather = require('./weatherRouter')
 const { authenticate } = require('../middleware/auth')
 
 router.use('/user',userRouter)
-router.use(authenticate)
+// router.use(authenticate)
 router.use('/song', songRouter)
+router.use('/weather', weather)
 
 module.exports = router
