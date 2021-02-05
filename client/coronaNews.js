7453361c4aec3bd26c0dbba825950c4a20c2d084
Edwin Dharmawan <edwindharmawan@gmail.com>
2021-02-05 14:49:14 +0700

    final commit

--- client/coronaNews.js
+++ client/coronaNews.js
@@ -10,10 +10,11 @@ function coronaNews() {
     $('#coronaNews').empty()
     $('#coronaNews').append(`
       <div class="headline mb-3">
-        <h5> <b> Latest Corona Information Indonesia </b> </h5>
+        <h3> <b> MUSIC <br> FOR EVERYONE </b> </h3>
       </div>
       <marquee behavior="scroll" direction="right">
         <div id="details" class="d-flex justify-content-center gap-4">
+          <p> <b> Latest corona info</b> </p>
           <p>Confirmed: <b>${ numberThousand(response[0].confirmed)}</b> </p>
           <p>Recovered: <b>${numberThousand(response[0].recovered)}</b></p>
           <p>Death: <b>${numberThousand(response[0].deaths)}</b></p>
